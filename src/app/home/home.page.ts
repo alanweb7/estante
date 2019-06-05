d003671960e4d46192cd24050ab98f14d1065b3d
Alan Silva <alanweb7@gmail.com>
2019-06-05 17:14:35 -0300

    Initial commit 3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from '../usuario.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [UsuarioService]
 })
+
 export class HomePage {
 
+  public usuarios = [];
+  public usuarioCadastro = {"nome":"", "idade":null};
+
+  constructor( private usuarioService:UsuarioService){
+    this.getUsuarios();
+  }
+
+  public getUsuarios(){
+
+    this.usuarioService.findAll().subscribe(response => this.usuarios = response );
+
+  }
+
+  public salvarUsuario(){
+    this.usuarioService.salvar(this.usuarioCadastro).subscribe(response => this.getUsuarios());
+  }
+  
 }
